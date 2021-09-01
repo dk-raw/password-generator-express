c1b0c93ecbc3912ebada3592dcdb10a887016f07
dk-raw <ddkatsios@protonmail.com>
2021-09-01 12:31:26 +0300

    404 and 500

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log('Server listening on port 3000')
+    console.log(`Server listening on port ${PORT}`)
 });
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -24,9 +24,19 @@ app.get('/', (req, res) => {
     /* res.download('server.js'); */
     /* res.sendFile('server.js', { root: __dirname }); displays a file in the browser */
     /* res.render('index'); */
+    /* res.redirect('https://google.com'); */
     const password = '12345';
     res.render('index.ejs', {
         title: 'Strong Password Generator',
         password: generatePassword(16)
     });
+});
+
+app.use((req, res) => {
+    res.status(404).json('404 Not Found');
+});
+
+app.use((err, req, res) => {
+    console.error(err.stack);
+    res.status(500).json('500 Internal Server Error');
 });
